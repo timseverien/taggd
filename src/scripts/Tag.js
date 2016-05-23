3aaf6d297a2d709e6e1816a9e2ca9d7744d6f6ce
Tim Severien <timseverien@gmail.com>
2016-05-23 22:37:32 +0200

    Add Taggd.Tag methods

--- src/scripts/Tag.js
+++ src/scripts/Tag.js
@@ -1,10 +1,63 @@
+const ObjectIs = require('./util/object-is');
+const TypeErrorMessage = require('./util/type-error-message');
+
 /**
  * @todo:
- * - Create methods to set behavior, text and position
  * - Generate DOM
  */
 class Tag {
+  constructor() {
+    this.setPosition(0, 0);
+    this.setText('');
+  }
 
+  /**
+   * Show the tag
+   * @return {Taggd.Tag} Current Tag
+   */
+  show() {
+    return this;
+  }
+
+  /**
+   * Hide the tag
+   * @return {Taggd.Tag} Current Tag
+   */
+  hide() {
+    return this;
+  }
+
+  /**
+   * Set the tag’s text
+   * @param {String} text - The tag’s content
+   * @return {Taggd.Tag} Current Tag
+   */
+  setText(text) {
+    if (!ObjectIs.ofType(text, 'string') && ObjectIs.function(text)) {
+      throw new Error(TypeErrorMessage.getMessage(type, 'a string or a function'));
+    }
+
+    this.text = text;
+    return this;
+  }
+
+  /**
+   * Set the tag’s position
+   * @param {Number} x - The tag’s x-coordinate
+   * @param {Number} y - The tag’s y-coordinate
+   * @return {Taggd.Tag} Current Tag
+   */
+  setPosition(x, y) {
+    if (!Number.isNumber(x)) {
+      throw new Error(TypeErrorMessage.getIntegerMessage(x));
+    }
+    if (!Number.isNumber(y)) {
+      throw new Error(TypeErrorMessage.getIntegerMessage(y));
+    }
+
+    this.position = {x, y};
+    return this;
+  }
 }
 
 module.exports = Tag;
--- src/scripts/util/object-is.js
+++ src/scripts/util/object-is.js
@@ -10,6 +10,16 @@ module.exports = {
   },
 
   /**
+   * Check whether an object is equals to given type
+   * @param {Object} object - The object to test
+   * @param {String} type - The type to test
+   * @return {Boolean}
+   */
+  ofType: (object, type) => {
+    return typeof object === type;
+  },
+
+  /**
    * Check whether given object is a function
    * @param {Object} object - The object to test
    * @return {Boolean}
